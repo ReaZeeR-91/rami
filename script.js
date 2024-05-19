fef42953410c710a07c60b347c8430250ae7490a
ReaZeeR <kekesasa47@gmail.com>
2024-05-19 23:11:18 +0200

    Update WebSocket URL to Render

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const startScreen = document.getElementById('startScreen');
     const waitingScreen = document.getElementById('waitingScreen');
     const gameContainer = document.getElementById('gameContainer');
-    let socket;
+    const socket = new WebSocket('https://api.render.com/deploy/srv-cp56j9779t8c73eprf90?key=836qA3F07E4');
 
     nameForm.addEventListener('submit', (event) => {
         event.preventDefault();
